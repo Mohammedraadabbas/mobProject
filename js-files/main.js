02055a953158cfbdf9d22af6dd9aa373e3f21b47
Mohammedraadabbas <104260916+Mohammedraadabbas@users.noreply.github.com>
2022-08-02 02:07:47 -0700

    Delete js-files directory

--- js-files/main.js
+++
@@ -1,8 +0,0 @@
-let menu = document.querySelector(".menu") ,
- navBar = document.querySelector(".navBar") ;
-
- menu.addEventListener("click",function(){
-    menu.classList.toggle("active")
-    navBar.classList.toggle("active")
-    console.log(menu)
- })
