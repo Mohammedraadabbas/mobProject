fa0ab177d7aee9c48df970964b83e934d766c560
Mohammedraadabbas <104260916+Mohammedraadabbas@users.noreply.github.com>
2022-08-04 08:56:42 -0700

    Update main.js

--- js-files/main.js
+++ js-files/main.js
@@ -6,3 +6,33 @@ let menu = document.querySelector(".menu") ,
     navBar.classList.toggle("active")
     console.log(menu)
  })
+
+
+const wrapper = document.querySelector('.wrapper');
+
+
+let pressed = false;
+let startX = 0;
+
+wrapper.addEventListener("mousedown", function(e)  {
+  pressed = true
+  startX= e.clientX;
+  this.style.cursor = "grabbing"
+  console.log(startX)
+})
+
+wrapper.addEventListener("mouseleave", function(e)  {
+  pressed = false;
+})
+
+window.addEventListener("mouseup", function(e)  {
+  pressed = false;
+  wrapper.style.cursor = "grab"
+})
+
+wrapper.addEventListener("mousemove", function(e)  {
+  if(!pressed){
+    return;
+  }
+  this.scrollLeft += startX - e.clientX;
+})
